28ad9e051cf0b82704ce26ab241611ce83b93649
viper101265 <s2027638@online.houstonisd.org>
2026-01-06 15:09:30 -0600

    Create package.js

---
+++ server/package.js
@@ -0,0 +1,15 @@
+{
+  "name": "unfair-chess",
+  "version": "1.0.0",
+  "description": "Unfair Chess™ – AI cheats, player obeys rules 🗿💀",
+  "main": "index.js",
+  "scripts": {
+    "start": "node server/index.js"
+  },
+  "author": "You",
+  "license": "MIT",
+  "dependencies": {
+    "express": "^4.18.2",
+    "openai": "^4.29.0"
+  }
+}
